ecc1aba84d92fd559436b773c378d24c2f525e55
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-10 20:17:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz(number)
   if number % 3 == 0
     return "Fizz"
+  elsif number % 5 == 0
+    return "Buzz"
+  elsif number % 15 == 0
+    return "FizzBuzz"
+  else
+    return nil
   end
 end
